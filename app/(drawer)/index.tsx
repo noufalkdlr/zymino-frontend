76ae82ca610809061908b71f463200ff313e446c
Noufal <noufalkakdlr@gmail.com>
2026-02-13 20:53:34 +0530

    fix bug

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { useAuthStore } from '@/src/store/useAuthStore';
 
-export default function Index() {
+export default function HomePage() {
+  const { logout } = useAuthStore()
   return (
     <View>
       <Text>Hello</Text>
-      <Text></Text>
+      <TouchableOpacity className='bg-red-500 px-8 py-4' onPress={logout}>
+        <Text className='text-white'>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
