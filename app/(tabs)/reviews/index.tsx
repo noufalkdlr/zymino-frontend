8c8f6969dd610267cc9e1856ad36b1762e190351
Noufal <noufalkakdlr@gmail.com>
2026-03-13 23:06:30 +0530

    add theme switch

--- app/(tabs)/reviews/index.tsx
+++ app/(tabs)/reviews/index.tsx
@@ -1,9 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useThemeStore } from "@/src/store/useThemeStore";
+import { useColorScheme } from "nativewind";
 
 export default function ReviewPage() {
+  const { theme, setTheme } = useThemeStore();
+
+  const { colorScheme } = useColorScheme();
+
+  const toggleTheme = () => {
+    if (colorScheme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
   return (
-    <View>
-      <Text>Hello</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+
+      <Text className="text-3xl font-bold text-text mb-4">
+        Hello Noufal! 👋
+      </Text>
+
+      <Text className="text-lg text-text mb-8">
+        Current Mode: {colorScheme}
+      </Text>
+      <TouchableOpacity
+        onPress={toggleTheme}
+        className="px-6 py-3 bg-primary rounded-xl active:opacity-80"
+      >
+        <Text className="text-white font-semibold text-base">
+          Toggle Theme 🌗
+        </Text>
+      </TouchableOpacity>
+
     </View>
-  )
+  );
 }
